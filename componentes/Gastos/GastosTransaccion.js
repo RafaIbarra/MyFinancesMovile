366668b4105481a47e6c961f27fb642623a920ce
RafaIbarra <blasrafael1986@gmail.com>
2024-05-03 15:57:04 -0400

    mejora modal busqueda

--- componentes/Gastos/GastosTransaccion.js
+++ componentes/Gastos/GastosTransaccion.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect,useContext} from "react";
 import { useRoute } from "@react-navigation/native";
+// import Animated from 'react-native-reanimated';
 // import { useNavigation } from '@react-navigation/native';
 import {  StyleSheet,View,TouchableOpacity,TextInput,Text,Modal,Pressable } from "react-native";
 import { Button } from 'react-native-paper';
@@ -11,8 +12,10 @@ import moment from 'moment';
 import { AntDesign } from '@expo/vector-icons'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-
 import { FontAwesome } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
+
+
 import Handelstorage from "../../Storage/handelstorage";
 import Generarpeticion from "../PeticionesApi/apipeticiones";
 import { AuthContext } from "../../AuthContext";
@@ -22,13 +25,18 @@ import { FlatList, ScrollView } from "react-native-gesture-handler";
 function GastosTransaccion({ navigation }){
     const { activarsesion, setActivarsesion } = useContext(AuthContext);
     const { colors } = useTheme();
-
+    
     const {params: { item },} = useRoute();
 
     const [isFocusedobs, setIsFocusedobs] = useState(false);
     const [isFocusedgasto, setIsFocusedgasto] = useState(false);
     const [estadomodal,setEstadomodal]=useState(false)
     const [datamodal,setDatamodal]=useState([])
+    const [datamodalcompleto,setDatamodalcompleto]=useState([])
+    const [textobusquedamodal,setTextobusquedamodal]=useState('')
+    const [isFocusemodal, setIsFocusedmodal] = useState(false);
+    const [modomodal, setModomodal] = useState(0);
+
     
     
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
@@ -38,7 +46,7 @@ function GastosTransaccion({ navigation }){
     const [listagastos,setListagastos]=useState([])
     const [codigoregistro,setCodigoregisto]=useState(0)
     const [optionscategoria, setOptionscategoria] = useState([]);
-    const[categoriasel,setCategoriasel]=useState(0)
+    const[categoriasel,setCategoriasel]=useState([])
     const [selectedOptioncategoria, setSelectedOptioncategoria] = useState(null);
     const [optionsgasto, setOptionsgasto] = useState([]);
     const [selectedOptiongasto, setSelectedOptiongasto] = useState(null);
@@ -52,9 +60,50 @@ function GastosTransaccion({ navigation }){
 
     const toggleModal = () => {
         setEstadomodal(!estadomodal);
+        
+       
+        
+      };
+    const abrircategoria =()=>{
+        setModomodal(1)
+        toggleModal()
+        
         console.log(optionscategoria)
         setDatamodal(optionscategoria)
-      };
+        setDatamodalcompleto(optionscategoria)
+    }
+    const seleccionopcionmodal=(itemsel)=>{
+        console.log('item seleccion')
+        console.log(itemsel.id)
+        if(modomodal===1){
+
+            setCategoriasel(itemsel)
+            setSelectedOptioncategoria(itemsel.id)
+        }else{
+            console.log('gasto')
+        }
+
+        toggleModal()
+
+    }
+    const abrirgasto =()=>{
+        setModomodal(2)
+        toggleModal()
+        console.log(optionscategoria)
+        setDatamodal(optionscategoria)
+        setDatamodalcompleto(optionscategoria)
+    }
+    
+
+    const realizarbusquedamodal= (palabra)=>{
+        setTextobusquedamodal(palabra)
+        const pal =palabra.toLowerCase()
+        let arrayencontrado = datamodalcompleto.filter(item => 
+          item.opcion.toLowerCase().includes(pal)
+          
+          );
+        setDatamodal(arrayencontrado)
+      }
 
     
     const handleGoBack = () => {
@@ -206,7 +255,7 @@ function GastosTransaccion({ navigation }){
 
     if(realizado){
         return(
-            <View style={{flex: 1,justifyContent:'center'}}>
+            <View style={{flex: 1,justifyContent:'flex-start',marginTop:75}}>
                   
                   
                   <ScrollView style={{padding:10,
@@ -241,10 +290,35 @@ function GastosTransaccion({ navigation }){
                           
                           <Text style={[styles.inputtextactivo, 
                                         { width:'50%',
-                                          color: fechaegreso ? colors.text : 'gray',
-                                          borderBottomColor: fechaegreso ? colors.textbordercoloractive : colors.textbordercolorinactive}]} 
+                                          color: categoriasel.opcion ? colors.text : 'gray',
+                                          borderBottomColor: categoriasel.id ? colors.textbordercoloractive : colors.textbordercolorinactive}]} 
                             onPress={showDatePicker} >
-                            {fechaegreso ? moment(fechaegreso).format('DD/MM/YYYY') : 'Categoria..'}
+                            {categoriasel.opcion ? categoriasel.opcion : 'Seleccione Categoria'}
+
+                            
+                            
+                            </Text>
+        
+                          <TouchableOpacity 
+                            style={styles.botonfecha} 
+                            onPress={abrircategoria}>         
+                              <FontAwesome name="search-plus" size={30} color={colors.iconcolor} />
+                          </TouchableOpacity>
+        
+                          
+        
+                      </View>
+
+                      <View style={{ flexDirection: 'row', alignItems:'stretch' }}>
+                          
+                          <Text style={[styles.inputtextactivo, 
+                                        { width:'50%',
+                                          color: categoriasel.opcion ? colors.text : 'gray',
+                                          borderBottomColor: categoriasel.id ? colors.textbordercoloractive : colors.textbordercolorinactive}]} 
+                            onPress={showDatePicker} >
+                            {categoriasel.opcion ? categoriasel.opcion : 'Seleccione Gasto'}
+
+                            
                             
                             </Text>
         
@@ -257,7 +331,7 @@ function GastosTransaccion({ navigation }){
                           
         
                       </View>
-                      <RNPickerSelect
+                      {/* <RNPickerSelect
                               onValueChange={SeleccionGasto}
                               items={optionsgasto}
                               value={selectedOptiongasto}
@@ -274,7 +348,7 @@ function GastosTransaccion({ navigation }){
                                 
                                 // Más props aquí...
                               }}
-                            />
+                            /> */}
                       <TextInputMask
                                   style={[styles.inputtextactivo,{color: colors.text,backgroundColor:colors.backgroundInpunt, borderBottomColor: isFocusedgasto ? colors.textbordercoloractive : colors.textbordercolorinactive }]}
                                   type={'money'}
@@ -355,43 +429,64 @@ function GastosTransaccion({ navigation }){
                   </Button>
 
                   
-                    <Modal
-                    visible={estadomodal}
-                    transparent={true}
-                    onRequestClose={toggleModal}
-                    >
+                    <Modal visible={estadomodal} 
+                            transparent={true} 
+                            onRequestClose={toggleModal}
+                            animationType="slide" 
+                            // animationDuration={2000}
+                            >
                         <TouchableOpacity
                             style={styles.overlay}
                             activeOpacity={1}
                             onPress={toggleModal}
-                            />
-                        <View style={styles.modalContainer}>
-                        <Text>Contenido del modal</Text>
-                        <TextInput placeholder="Buscar.." placeholderTextColor={'red'}> </TextInput>
-                        
-                        <FlatList
-                        data={datamodal}
-                        renderItem={({item}) =>{
-                            return(
-                                
-                                    <View>
-                                        <Text style={{color:'black'}}>  {item.opcion}</Text>
-                                    </View>
-                                
-                                
-                            )
-                        }
-                    }
-                        keyExtractor={item => item.id}
-
                         />
-                            
 
                         
-                        <TouchableOpacity onPress={toggleModal}>
-                            <Text style={{ color: 'blue', marginTop: 10 }}>Cerrar modal</Text>
-                        </TouchableOpacity>
-                        </View>
+
+
+                            
+                            <View style={[styles.modalContainer,{backgroundColor:'rgb(28,44,52)'}]}>
+                                <View style={{alignItems:'center',marginBottom:30,marginTop:5}}>
+                                    <View style={{borderBottomWidth: 3,borderBottomColor: 'white',width:300,marginVertical: 10,}}></View>
+                                    <View style={{borderBottomWidth: 1,borderBottomColor: 'white',width:250,marginVertical: 3,}}></View>
+                                    <View></View>
+                                </View>
+                                <TextInput style={[{borderBottomWidth: 2,marginBottom:10,color: colors.text,backgroundColor:colors.backgroundInpunt, borderBottomColor: isFocusemodal ? colors.textbordercoloractive : colors.textbordercolorinactive }]}
+                                    placeholder="Buscar Categoria..." 
+                                    placeholderTextColor='gray'
+                                    value={textobusquedamodal}
+                                    onChangeText={textobusquedamodal => realizarbusquedamodal(textobusquedamodal)}
+                                    onFocus={() => setIsFocusedmodal(true)}
+                                    onBlur={() => setIsFocusedmodal(false)}
+                                > 
+                                    
+                                </TextInput>
+                                <View style={{borderTopColor:colors.bordercolor,borderWidth:2,marginTop:10}} >
+
+                                    <FlatList
+                                    data={datamodal}
+                                    renderItem={({item}) =>{
+                                        return(
+                                                <View style={{marginLeft:15,borderBottomWidth:1,borderBottomColor:'white',marginBottom:10,padding:10}} >
+
+                                                    <TouchableOpacity onPress={()=>seleccionopcionmodal(item)}>
+                                                        <Text style={{color:colors.text}}>{item.opcion}</Text>
+                                                    </TouchableOpacity>
+                                                </View>
+                                                )
+                                        }
+                                    }
+                                    keyExtractor={item => item.id}
+
+                                    />
+                                </View>
+                        
+                            </View>
+                        
+
+
+
+
                     </Modal>
                 
                
@@ -415,15 +510,23 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
       },
     modalContainer: {
-    backgroundColor: 'white',
-    padding: 20,
+    
+    // padding: 20,
+    paddingBottom:20,
+    paddingLeft:20,
+    paddingRight:20,
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
     height:'50%',
     borderTopLeftRadius:50,
-    borderTopRightRadius:50
+    borderTopRightRadius:50,
+    borderLeftWidth:1,
+    borderTopWidth:1,
+    borderRightWidth:1,
+    
+    borderColor:'gray'
     },
     
     input: {
