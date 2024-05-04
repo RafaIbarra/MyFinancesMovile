77963a7d7d51853f487b4c4c36534074c7e60036
RafaIbarra <blasrafael1986@gmail.com>
2024-05-04 11:51:40 -0400

    inicio componente resumen

--- componentes/Resumen/Resumen.js
+++ componentes/Resumen/Resumen.js
@@ -1,15 +1,22 @@
-import React,{useState,useEffect } from "react";
+import React,{useState,useEffect,useContext } from "react";
 import { ActivityIndicator, View,Text,StyleSheet,TextInput  } from "react-native";
 import {DefaultTheme, Provider} from 'react-native-paper';
 import { useTheme } from '@react-navigation/native';
 
 import { TouchableOpacity, FlatList } from 'react-native';
 
+import Handelstorage from "../../Storage/handelstorage";
+import Generarpeticion from "../PeticionesApi/apipeticiones";
+import { AuthContext } from "../../AuthContext";
 
-function Resumen (){
+function Resumen ({ navigation  }){
   const { colors } = useTheme();
+  const { activarsesion, setActivarsesion } = useContext(AuthContext);
   const [expandedItem, setExpandedItem] = useState(null);
+  const [datadetalle,setDatadetalle]=useEffect([])
+  const [cargacompleta,setCargacopleta]=useState(false)
   const [searchValue, setSearchValue] = useState('');
+
   const data = [
     { 
       id: 1, 
@@ -77,22 +84,71 @@ function Resumen (){
       </View>
     );
   }
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      
+      const cargardatos=async()=>{
+          const datestorage=await Handelstorage('obtenerdate');
+          const mes_storage=datestorage['datames']
+          const anno_storage=datestorage['dataanno']
+          const body = {};
+          const endpoint='MovileResumenMes/' + anno_storage +'/' + mes_storage + '/'
+          const result = await Generarpeticion(endpoint, 'POST', body);
+          const respuesta=result['resp']
+          if (respuesta === 200){
 
-    return(
-        
+              console.log(result['data']['datos'])
+              
+              // const detalle=result['data']['datos'].filter(item => item.Codigo !== 3);
+              // console.log(detalle)
+              // setDatadetalle(detalle)
+              
+              
+              
+              
+          }else if(respuesta === 403 || respuesta === 401){
+              
+              
+              await Handelstorage('borrar')
+              await new Promise(resolve => setTimeout(resolve, 1000))
+              setActivarsesion(false)
+          }
+          
+          setCargacopleta(true)
 
-        <View style={styles.container} >
-            <Text style={{ color: colors.text }}> Resumen del mes aca prueba colores Anita</Text>
-            <FlatList
-      data={data}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id.toString()}
-    />
-            
-        </View>
-        
-    )
+         
+      }
+      cargardatos()
+      // setRefresh(false)
+    })
+    return unsubscribe;
+    }, [navigation]);
 
+    if(cargacompleta){
+          return(
+              
+
+              <View style={styles.container} >
+                  <Text style={{ color: colors.text }}> Resumen del mes aca prueba colores Anita</Text>
+                  {/* <FlatList
+            data={datadetalle}
+            rrenderItem={({ item }) => (
+              <View>
+                <Text>{item.Descripcion}</Text>
+                <Text>Código: {item.Codigo}</Text>
+                <Text>Monto de Egreso: {item.MontoEgreso}</Text>
+                <Text>Monto de Ingreso: {item.MontoIngreso}</Text>
+                <Text>Saldo: {item.Saldo}</Text>
+                <Text>Tipo: {item.Tipo}</Text>
+              </View>
+            )}
+            keyExtractor={item => item.Descripcion.toString()}
+          /> */}
+                  
+              </View>
+              
+          )
+    }
 
 
 }
